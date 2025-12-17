fa5a2cff2e726708f2a6ed724aef898a30bc7ac9
kanukuntla-R <197695171+kanukuntla-R@users.noreply.github.com>
2025-12-16 23:48:28 -0700

    2025-12-16 : auto commit

--- frontend/src/pages/Chatbot.jsx
+++ frontend/src/pages/Chatbot.jsx
@@ -3,6 +3,14 @@ import TabBar from "../components/TabBar.jsx";
 
 export default function Chatbot() {
   return (
+
+
+
+
+
+
+
+    
     <div className="page">
       <div className="sectionTitle">Chat bot</div>
       <div className="card">Chat bot coming soon.</div>
