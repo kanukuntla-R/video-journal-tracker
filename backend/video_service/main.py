7632ae66130726d1c846f66925ae38b90634b768
kanukuntla-R <197695171+kanukuntla-R@users.noreply.github.com>
2025-12-22 18:06:11 -0700

    to run in local

--- backend/video_service/main.py
+++ backend/video_service/main.py
@@ -13,6 +13,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
+    "http://192.168.0.20:5173",
     # Replace with your LAN IP (use `ifconfig`/`ipconfig`) so phone/tablet can call the API
     "http://10.155.225.19:5173",
 ]
